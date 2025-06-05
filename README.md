84cfca951ea685a3e367d3122d839d5553f8f06c
StevenLe456 <stevenhle456@outlook.com>
2025-06-05 11:07:50 -0500

    Created the backbone of the application

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # AccessibleRacingGame
-CSCE.3444 Group 6 Project
+
+## Brief Description
+
+This is the Group 6 project for CSCE.3444 at the University of North Texas during the Summer 2025 semester with Professor Hadish Gooranorimi.
+
+## Development Documentation
+
+### List of directories and files
+
+* public/ - Contains external files (ex. HTML, CSS, images, etc.)
+    * index.html - The main HTML file
+    * style.css - The main styling sheet
+* src/ - Contains the Typescript files
+    * game.ts - Contains the game rendering code
+    * main.ts - Contains the main loop
+* .gitignore - For Git purposes
+* package-lock.json - Do not edit
+* package.json - Contains npm and Javascript-related metadata and configuration
+* README.md - You are here
+* tsconfig.json - Contains Typescript-related metadata and configuration
+
+### How to Build and Run
+
+To build a non-production build, create a folder called `test` and run the following command:
+`npm run test`
+
+To build a production build, create a folder called `dist` and run the following command:
+`npm run dist`
+
+Then, go into the respective folder and use a webserver to open up the web app.
---
+++ src/game.ts
@@ -0,0 +1,34 @@
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+
+export class Game {
+    constructor() {
+        // get canvas
+        var canvas = <HTMLCanvasElement> document.getElementById("gameCanvas")
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && (ev.key === "I" || ev.key === "i")) {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                } else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+
+        // run the main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+    }
+}
