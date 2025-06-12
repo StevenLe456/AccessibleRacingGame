b911b102ae9fb8732fab763aed45af8e53cb659b
StevenLe456 <stevenhle456@outlook.com>
2025-06-12 16:55:11 -0500

    Added two cars to the scene

--- src/car.ts
+++ src/car.ts
@@ -1,4 +1,4 @@
-import { Scene, Mesh, MeshBuilder, UniversalCamera, Vector3 } from "@babylonjs/core"
+import { Scene, Mesh, MeshBuilder, UniversalCamera, Vector3, Viewport } from "@babylonjs/core"
 import { Color4 } from "babylonjs"
 
 export class Car {
@@ -9,12 +9,12 @@ export class Car {
     private green
     private blue
     private scene
-    private canvas
+    private cvs_x
     private id
     private mesh: Mesh
     private cam: UniversalCamera
 
-    constructor(x: number, y: number, z: number, r: number, g: number, b: number, s: Scene, cvs: HTMLCanvasElement, id: string) {
+    constructor(x: number, y: number, z: number, r: number, g: number, b: number, s: Scene, cvs_x: number, id: string) {
         this.x = x
         this.y = y
         this.z = z
@@ -22,12 +22,14 @@ export class Car {
         this.green = g
         this.blue = b
         this.scene = s
-        this.canvas = cvs
+        this.cvs_x = cvs_x
         this.id = id
         var c: Color4 = new Color4(this.red, this.green, this.blue, 1.0)
         this.mesh = MeshBuilder.CreateBox("car" + this.id, {width: 1, height: 1, depth: 3, faceColors: [c, c, c, c, c, c], updatable: true}, this.scene)
         this.mesh.position = new Vector3(this.x, this.y, this.z)
         this.cam = new UniversalCamera("cam" + id, new Vector3(this.x, this.y + 2, this.z - 10), this.scene)
+        this.scene.activeCameras?.push(this.cam)
+        this.cam.viewport = new Viewport(this.cvs_x, 0, 0.5, 1)
     }
 
     turn_left() {
--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, Vector3, HemisphericLight } from "@babylonjs/core"
+import { Engine, Scene, Vector3, HemisphericLight, Mesh, MeshBuilder} from "@babylonjs/core"
 
 import { Car } from "./car"
 import {InputHandler} from "./input"
@@ -8,18 +8,22 @@ export class Game {
     private engine
     private scene
     private car1: Car
+    private car2: Car
     private inputty: InputHandler
 
     constructor() {
         // get canvas
         this.canvas = <HTMLCanvasElement> document.getElementById("gameCanvas")
-
+        
         // initialize babylon scene and engine
         this.engine = new Engine(this.canvas, true)
         this.scene = new Scene(this.engine)
 
-        // add car and light to scene
-        this.car1 = new Car(0, 0, 0, 1, 0, 0, this.scene, this.canvas, "1")
+        // add stuff to scene
+        this.car1 = new Car(-10, 0, 0, 1, 0, 0, this.scene, 0, "1")
+        this.car2 = new Car(10, 0, 0, 0, 1, 0, this.scene, 0.5, "2")
+        var track: Mesh = MeshBuilder.CreateBox("racetrack", {width: 40, height: 0.01, depth: 1000})
+        track.position = new Vector3(0, -0.005, 450)
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene)
 
         // initialize input handler
