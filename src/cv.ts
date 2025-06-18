648c9359bb685e3dfbf4328cc66e6aa5880c4f4f
StevenLe456 <stevenhle456@outlook.com>
2025-06-18 13:41:32 -0500

    Updated UI and working on CV module

--- src/cv.ts
+++ src/cv.ts
@@ -1,1 +1,12 @@
-import * as tf from '@tensorflow/tfjs';
+import '@mediapipe/face_mesh'
+import '@tensorflow/tfjs-core'
+import '@tensorflow/tfjs-backend-webgl'
+import * as face from '@tensorflow-models/face-landmarks-detection'
+
+const model = face.SupportedModels.MediaPipeFaceMesh
+const detectorConfig: face.MediaPipeFaceMeshMediaPipeModelConfig = {
+  runtime: 'mediapipe',
+  solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+  refineLandmarks: false
+}
+var detector = async () => {await face.createDetector(model, detectorConfig)}
