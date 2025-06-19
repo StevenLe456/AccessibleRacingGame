3421ffec538700ad5e8b8f92a11200e468ba8fd1
StevenLe456 <stevenhle456@outlook.com>
2025-06-18 20:52:31 -0500

    Wrote functions for CV module

--- src/cv.ts
+++ src/cv.ts
@@ -3,10 +3,33 @@ import '@tensorflow/tfjs-core'
 import '@tensorflow/tfjs-backend-webgl'
 import * as face from '@tensorflow-models/face-landmarks-detection'
 
-const model = face.SupportedModels.MediaPipeFaceMesh
-const detectorConfig: face.MediaPipeFaceMeshMediaPipeModelConfig = {
-  runtime: 'mediapipe',
-  solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-  refineLandmarks: false
+export async function load(): Promise<face.FaceLandmarksDetector> {
+    const model = face.SupportedModels.MediaPipeFaceMesh
+    const detectorConfig: face.MediaPipeFaceMeshMediaPipeModelConfig = {
+        runtime: 'mediapipe',
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+        refineLandmarks: false
+    }
+    const detector = await face.createDetector(model, detectorConfig)
+    return detector
 }
-var detector = async () => {await face.createDetector(model, detectorConfig)}
+
+export function initVideo(): HTMLVideoElement {
+    var video: HTMLVideoElement = <HTMLVideoElement> document.getElementById("videoElement")
+    if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream
+        })
+        .catch(function (error) {
+            console.log("Something went wrong!")
+        })
+    }
+    return video
+}
+
+export async function faceMesh(detector: face.FaceLandmarksDetector, video: HTMLVideoElement): Promise<Object[]> {
+    const estimationConfig = {flipHorizontal: false}
+    const faces = await detector.estimateFaces(video, estimationConfig)
+    return faces
+}
