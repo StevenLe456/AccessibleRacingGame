c88af02977643a602d4afba25175bbf1eaf88f50
StevenLe456 <stevenhle456@outlook.com>
2025-06-22 18:03:09 -0500

    Getting rid of unnecessary files

--- src/comp_vision.ts
+++ src/comp_vision.ts
@@ -1,11 +1,8 @@
-import * as tf from '@tensorflow/tfjs';
-
 import '@mediapipe/face_detection';
 import '@tensorflow/tfjs-core';
-// Register WebGL backend.
+// Adds the WebGL backend to the global backend registry.
 import '@tensorflow/tfjs-backend-webgl';
 import * as faceDetection from '@tensorflow-models/face-detection';
-
 
 export async function initFaceModel() {
     const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
@@ -20,13 +17,4 @@ export async function initFaceModel() {
 export async function getFace(detector: faceDetection.FaceDetector, video: HTMLCanvasElement) {
     const estimationConfig = {flipHorizontal: false};
     return await detector.estimateFaces(video, estimationConfig);
-}
-
-export async function initModel() {
-    return await tf.loadLayersModel('model/model.json')
-}
-
-export function getPred(model: tf.LayersModel, video: HTMLCanvasElement) {
-    const data = tf.browser.fromPixels(video).expandDims(0)
-    return model.predict(data)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import * as tf from '@tensorflow/tfjs';
-import { initFaceModel, getFace, initModel, getPred } from "./comp_vision"
+import '@mediapipe/face_detection';
+import '@tensorflow/tfjs-core';
+// Adds the WebGL backend to the global backend registry.
+import '@tensorflow/tfjs-backend-webgl';
+import { initFaceModel, getFace } from "./comp_vision"
 import { Game } from "./game"
-
-// let model = initModel()
 
 let model = initFaceModel()
 
 let video: HTMLVideoElement = <HTMLVideoElement> document.getElementById("stream")
-const canvas = <HTMLCanvasElement> document.getElementById('canvas');
-const ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
+const canvas = <HTMLCanvasElement> document.getElementById('canvas')
+const ctx = <CanvasRenderingContext2D> canvas.getContext('2d')
 navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {video.srcObject = stream})
 
 let x = <HTMLElement> document.getElementById("x")
 let y = <HTMLElement> document.getElementById("y")
 
 async function main() {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
     model.then(function(m) {
         getFace(m, canvas).then(function(d) {
             console.log(d[0]["keypoints"][2])
@@ -25,28 +26,6 @@ async function main() {
 }
 
 main()
-
-// let idxs = []
-// for (let i = 0; i < 66; i++) {
-//     idxs.push(i)
-// }
-// let idx_tensor = tf.tensor(idxs)
-
-// async function main() {
-//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-//     model.then(function(m) {
-//         let x: tf.Tensor[] = <tf.Tensor[]> getPred(m, canvas);
-//         let yaw = x[0]
-//         let pitch = x[1]
-//         let roll = x[2]
-//         tf.sum(yaw.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){y.innerHTML = "Yaw: " + n})
-//         tf.sum(pitch.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){p.innerHTML = "Pitch: " + n})
-//         tf.sum(roll.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){r.innerHTML = "Roll: " + n})
-//     })
-//     requestAnimationFrame(main) 
-// }
-
-// main()
 
 // initialize game
 // var game = new Game()
