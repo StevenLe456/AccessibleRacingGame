59c261a53aa45f4f0d26f0bef2c9274271e273b9
StevenLe456 <stevenhle456@outlook.com>
2025-06-22 16:31:03 -0500

    Using nose tip detection

--- src/comp_vision.ts
+++ src/comp_vision.ts
@@ -1,5 +1,27 @@
 import * as tf from '@tensorflow/tfjs';
 
+import '@mediapipe/face_detection';
+import '@tensorflow/tfjs-core';
+// Register WebGL backend.
+import '@tensorflow/tfjs-backend-webgl';
+import * as faceDetection from '@tensorflow-models/face-detection';
+
+
+export async function initFaceModel() {
+    const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
+    const detectorConfig = <faceDetection.MediaPipeFaceDetectorMediaPipeModelConfig> {
+        runtime: 'mediapipe',
+        maxFaces: 1,
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_detection',
+    };
+    return await faceDetection.createDetector(model, detectorConfig);
+}
+
+export async function getFace(detector: faceDetection.FaceDetector, video: HTMLCanvasElement) {
+    const estimationConfig = {flipHorizontal: false};
+    return await detector.estimateFaces(video, estimationConfig);
+}
+
 export async function initModel() {
     return await tf.loadLayersModel('model/model.json')
 }
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,52 @@
 import * as tf from '@tensorflow/tfjs';
-import { initModel, getPred } from "./comp_vision"
+import { initFaceModel, getFace, initModel, getPred } from "./comp_vision"
 import { Game } from "./game"
 
-let model = initModel()
+// let model = initModel()
 
-let y: HTMLElement = <HTMLElement> document.getElementById("yaw")
-let p: HTMLElement = <HTMLElement> document.getElementById("pitch")
-let r: HTMLElement = <HTMLElement> document.getElementById("roll")
+let model = initFaceModel()
+
 let video: HTMLVideoElement = <HTMLVideoElement> document.getElementById("stream")
 const canvas = <HTMLCanvasElement> document.getElementById('canvas');
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
 navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {video.srcObject = stream})
 
-let idxs = []
-for (let i = 0; i < 66; i++) {
-    idxs.push(i)
-}
-let idx_tensor = tf.tensor(idxs)
+let x = <HTMLElement> document.getElementById("x")
+let y = <HTMLElement> document.getElementById("y")
 
 async function main() {
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     model.then(function(m) {
-        let x: tf.Tensor[] = <tf.Tensor[]> getPred(m, canvas);
-        let yaw = x[0]
-        let pitch = x[1]
-        let roll = x[2]
-        tf.sum(yaw.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){y.innerHTML = "Yaw: " + n})
-        tf.sum(pitch.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){p.innerHTML = "Pitch: " + n})
-        tf.sum(roll.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){r.innerHTML = "Roll: " + n})
+        getFace(m, canvas).then(function(d) {
+            console.log(d[0]["keypoints"][2])
+        })
     })
-    requestAnimationFrame(main) 
+    requestAnimationFrame(main)
 }
 
 main()
+
+// let idxs = []
+// for (let i = 0; i < 66; i++) {
+//     idxs.push(i)
+// }
+// let idx_tensor = tf.tensor(idxs)
+
+// async function main() {
+//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     model.then(function(m) {
+//         let x: tf.Tensor[] = <tf.Tensor[]> getPred(m, canvas);
+//         let yaw = x[0]
+//         let pitch = x[1]
+//         let roll = x[2]
+//         tf.sum(yaw.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){y.innerHTML = "Yaw: " + n})
+//         tf.sum(pitch.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){p.innerHTML = "Pitch: " + n})
+//         tf.sum(roll.softmax().mul(idx_tensor)).data().then(function(x){return x[0] * 3 - 99}).then(function(n){r.innerHTML = "Roll: " + n})
+//     })
+//     requestAnimationFrame(main) 
+// }
+
+// main()
 
 // initialize game
 // var game = new Game()
