150739a0f5cd0043214c32803161d90cdc1e1a68
StevenLe456 <stevenhle456@outlook.com>
2025-06-24 13:18:53 -0500

    Got head calibration UI to work

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,38 @@ import { Head } from './head';
 
 // init stuff
 let video: HTMLVideoElement = <HTMLVideoElement> document.getElementById("videoElement")
+let other_video: HTMLVideoElement = <HTMLVideoElement> document.getElementById("webby")
 const canvas = <HTMLCanvasElement> document.getElementById('stream')
+const lefty = <HTMLCanvasElement> document.getElementById("left")
+const righty = <HTMLCanvasElement> document.getElementById("right")
+const l_ctx = <CanvasRenderingContext2D> lefty.getContext('2d')
+const r_ctx = <CanvasRenderingContext2D> righty.getContext('2d')
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d')
-navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {video.srcObject = stream})
+let arr: Promise<number[]> = navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {other_video.srcObject = stream; let {width, height} = stream.getVideoTracks()[0].getSettings(); return <number[]> [width, height];})
 let model = initFaceModel()
 
-// do head calibration
+function head_calibration() {
+    arr.then(function(a) {
+        l_ctx.drawImage(other_video, 0, 0, a[0] / 2, a[1], 0, 0, lefty.width, lefty.height)
+        r_ctx.drawImage(other_video, a[0] / 2, 0, a[0] / 2, a[1], 0, 0, righty.width, righty.height)
+    })
+    setTimeout(head_calibration, 1000 / 30);
+}
 
+head_calibration()
 
+/*
 // head calibration (instead of hard-coding values for head, do a calibration check)
 let head1 = new Head(200, 185, 215, 90, 110)
+
+// prepare for game
+let ui: HTMLElement = <HTMLElement> document.getElementById("head-calibration")
+ui.style.display = "none"
+let gamey: HTMLElement = <HTMLElement> document.getElementById("game")
+gamey.style.display = "block"
+navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {video.srcObject = stream})
 
 // initialize game
 var game = new Game(head1, model, video, canvas, ctx)
 game.update()
+*/ // uncomment later
